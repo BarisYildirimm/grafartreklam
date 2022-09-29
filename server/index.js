4e3da93eee5988f5ef9dea7c23e1fdf40393b86a
BarisYildirimm <72316936+BarisYildirimm@users.noreply.github.com>
2022-09-30 00:55:06 +0300

    update express

--- server/index.js
+++ server/index.js
@@ -27,8 +27,10 @@ app.use("/slogan", sloganRoutes);
 app.use("/principle", principleRoutes);
 
 const __dirname = path.resolve();
+
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
+
+  app.use(Express.static(path.join(__dirname, "client/build")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
