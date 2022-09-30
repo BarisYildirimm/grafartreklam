9e0524883ac380492cbdbc8d00d1475570cbf8be
BarisYildirimm <72316936+BarisYildirimm@users.noreply.github.com>
2022-09-30 04:39:23 +0300

    update

--- server/client/src/components/Blog/Blog.jsx
+++ server/client/src/components/Blog/Blog.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import "./blog.css"
-
+import { Button } from "@mui/material";
+import { useNavigate } from 'react-router-dom';
 export default function Blog(props){
-    
+    const history = useNavigate();
     const date = new Date(props.date).toLocaleDateString()
     return(
                 <div className="item">
@@ -14,6 +15,7 @@ export default function Blog(props){
                         <div><p href="#" className="title">{props.title}</p></div>
                         <p className="descBlog">{props.desc}</p>
                         <a href={`/posts/${props.id}`} className="more">DEVAMINI OKU →</a>
+                        <Button  className="viewButton" onClick={()=>history(`/posts/${props.id}`)}>Bak</Button>
                     </div>
                 </div>
      )
