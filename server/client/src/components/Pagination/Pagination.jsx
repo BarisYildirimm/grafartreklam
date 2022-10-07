6937acc9b38c106137837b8f60a35b8de7fd00b6
BarisYildirimm <72316936+BarisYildirimm@users.noreply.github.com>
2022-10-08 02:22:00 +0300

    update pagination

--- server/client/src/components/Pagination/Pagination.jsx
+++ server/client/src/components/Pagination/Pagination.jsx
@@ -12,7 +12,7 @@ function Pagination({postsPerPage,totalPosts,paginate}) {
         <ul className='pagination'>
             {pageNumbers.map(number =>(
                 <li key={number} className="page-item">
-                    <a onClick={()=> paginate(number)} href='http://www.grafartreklam.com/blog' target={"_self"} className='page-link'>
+                    <a onClick={()=> paginate(number)} href='#!' className='page-link'>
                         {number}
                     </a>
                 </li>
